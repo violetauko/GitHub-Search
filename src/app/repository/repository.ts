6c6d550ee41ea9ec1d98718c1fe1f470d434193e
violetauko <aukoviolet24@gmail.com>
2022-04-19 01:43:11 +0300

    Auto-generated commit [ci skip]

--- src/app/repository/repository.ts
+++
@@ -1,5 +0,0 @@
-export class Repository {
-    constructor(public name:string,public html_url:string,public language:string,public description:string ){
-
-    }
-}
--- src/app/user/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    constructor(
-        public avatar_url: string,
-        public login: string,
-        public followers_url: string,
-        public following_url: string){
-
-    }
-}
